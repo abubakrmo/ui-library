b61a74cb2393bc31a60096359db14116378801ae
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-04-04 11:05:19 +0100

    dropdown class and css imported in index js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './uiComponents/tooltip'
 import './uiComponents/styles/tootltip.css'
-
+import Dropdown from './uiComponents/dropdown'
+import './uiComponents/styles/dropdown.css'
 
 
 
@@ -9,3 +10,8 @@ import './uiComponents/styles/tootltip.css'
 
 const tooltip = new Tooltip(document.querySelector('.toolTip'))
 tooltip.toolTip();
+
+dropdowns.forEach(dropdown =>{
+    const instance = new Dropdown(dropdown)
+    instance.dropDown();
+})
