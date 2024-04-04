2b54035f4a6edb2c3ffb8050288de8d0e656af50
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-04-04 11:10:52 +0100

    snackbar class and css imported in index js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import Dropdown from './uiComponents/dropdown'
 import './uiComponents/styles/dropdown.css'
 import Navtab from './uiComponents/navtabs'
 import './uiComponents/styles/navtabs.css'
-
-
-
-
+import Snackbar from './uiComponents/snackbar'
+import './uiComponents/styles/snackbar.css'
 
 const tooltip = new Tooltip(document.querySelector('.toolTip'))
 tooltip.toolTip();
 
+const dropdowns = document.querySelectorAll('.dropDown');
 dropdowns.forEach(dropdown =>{
     const instance = new Dropdown(dropdown)
     instance.dropDown();
@@ -19,3 +18,10 @@ dropdowns.forEach(dropdown =>{
 
 const navtab = new Navtab(document.querySelector('.navtabs'))
 navtab.toggle();
+
+const snackbar = new Snackbar();
+snackbar.createSnackbar();
+const button = document.querySelector('.snackbar-trigger')
+button.addEventListener('click', ()=>{
+    snackbar.displaySnackbar("Hooray!")
+})
