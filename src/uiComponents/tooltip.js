7762cd031d9120ebf0d712d17c8d33e2961b2bfa
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-04-04 10:59:02 +0100

    function to display tooltip created

---
+++ src/uiComponents/tooltip.js
@@ -0,0 +1,21 @@
+export default class Tooltip{
+    constructor(element){
+        this.element = element;
+        this.message = element.getAttribute('data-message')
+    }
+
+    toolTip(){
+        const tip = document.createElement('div');
+        tip.classList.add('tip');
+        tip.textContent = this.message;
+        this.element.appendChild(tip);
+
+        this.element.addEventListener('mouseenter', ()=>{
+            tip.classList.add('active')
+        })
+
+        this.element.addEventListener('mouseleave', ()=>{
+            tip.classList.remove('active')
+        })
+    }
+}
