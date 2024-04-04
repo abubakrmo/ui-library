9e7f015b0fcd0e8d46649e66c54afbbeaa50844b
abubakrmo <abubakarmufutauoseni@gmail.com>
2024-04-04 11:01:57 +0100

    tooltip class imported in index js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Tooltip from './uiComponents/tooltip'
+import './uiComponents/styles/tootltip.css'
+
+
+
+
+
+
+
+const tooltip = new Tooltip(document.querySelector('.toolTip'))
+tooltip.toolTip();
